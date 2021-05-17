270352992888329761fba42b9fdfb856b9d2b4be
SirJorah <dofer1999@gmail.com>
2021-05-17 16:13:11 -0400

    Test step1 & step2

--- frobenius.c
+++ frobenius.c
@@ -1,4 +1,5 @@
 #include "frobenius.h"
+#include "primes.h"
 
 u_int64_t multipl;
 
@@ -23,6 +24,85 @@ void release_memory()
     	gcry_mpi_release(two);
 };
 
+
+//Step 1: test n for divisibility by primes <= 1 + n/2
+void step_1(const gcry_mpi_t num)
+{
+	if(gcry_mpi_cmp(zero, num) == 0 || gcry_mpi_cmp(one, num) == 0 || gcry_mpi_cmp(two, num) == 0)
+	{
+		printf("Number is prime, ");
+		return;
+	}
+		
+	gcry_mpi_t buff = gcry_mpi_new(0);
+	gcry_mpi_div(buff, NULL, num, two, 0);
+	gcry_mpi_add(buff, buff, one); //1 + n/2
+
+	gcry_mpi_t prime = gcry_mpi_new(0);
+	gcry_mpi_set_ui(prime, prime_list[0]);
+
+	gcry_mpi_t mod = gcry_mpi_new(0);
+
+	for(short i = 1; gcry_mpi_cmp(num, prime) >= 0; i++)
+	{
+		if(gcry_mpi_cmp(prime, num) == 0)
+		{
+			printf("Number is prime, ");
+			return;
+		}
+
+		gcry_mpi_set_ui(prime, prime_list[i]);
+	};
+
+	gcry_mpi_set_ui(prime, prime_list[0]);
+
+	//i will start from 1, because number should be odd
+	//prime_list[1] is 3
+	
+	for(short i = 0; gcry_mpi_cmp(buff, prime) >= 0; i++)
+	{
+		gcry_mpi_mod(mod, num, prime);
+
+		if(gcry_mpi_cmp(zero, mod) == 0)
+		{
+			printf("Number is composite, ");
+			return;
+		}
+		else
+		{
+			gcry_mpi_set_ui(prime, prime_list[i]);
+		}
+	}
+
+	printf("Number is probably prime, ");
+
+	gcry_mpi_release(buff);
+	gcry_mpi_release(prime);
+	gcry_mpi_release(mod);
+};
+
+//Step 2: test whether sqrt(n) is integer
+void step_2 (const gcry_mpi_t num)
+{
+	gcry_mpi_t buff = gcry_mpi_new(0);
+	gcry_mpi_t buff2 = gcry_mpi_new(0);
+	gcry_mpi_set(buff2, zero);
+
+	for(gcry_mpi_set(buff, one); gcry_mpi_cmp(buff2, num) <= 0; gcry_mpi_add(buff, buff, one))
+	{
+		gcry_mpi_mul(buff2, buff, buff);
+		if(gcry_mpi_cmp(buff2, num) == 0)
+		{
+			printf("Number is square\n");
+			return;
+		}
+	};
+
+	printf("Number is not square\n");
+
+	gcry_mpi_release(buff);
+	gcry_mpi_release(buff2);
+};
 
 /*void mult_mod(gcry_mpi_t rez_x, gcry_mpi_t rez_1, const gcry_mpi_t f_x, const gcry_mpi_t f_1, const gcry_mpi_t g_x, const gcry_mpi_t g_1)
 {
--- frobenius.h
+++ frobenius.h
@@ -7,8 +7,15 @@ extern gcry_mpi_t zero;
 extern gcry_mpi_t one;
 extern gcry_mpi_t two;
 
+extern gcry_mpi_t n;
+extern gcry_mpi_t b;
+extern gcry_mpi_t c;
+
 extern void set_params();
 extern void release_memory();
+//extern int is_square(gcry_mpi_t);
+extern void step_1(const gcry_mpi_t);
+extern void step_2(const gcry_mpi_t);
 
 //void mult_mod();
 
